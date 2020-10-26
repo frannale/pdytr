577910b3e6d9ace2b3ceeb48c18d868f48ea0064
Rodrigo Jara <rodrigojara1997@gmail.com>
2020-10-26 17:31:22 -0300

    COdigo inicial practica 3

---
+++ Practica 3/grpc-hello-server/src/main/java/pdytr/example/grpc/GreetingServiceImpl.java
@@ -0,0 +1,33 @@
+package pdytr.example.grpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+  @Override
+  public void greeting(GreetingServiceOuterClass.HelloRequest request,
+        StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+    // HelloRequest has toString auto-generated.
+    System.out.println(request);
+
+    System.out.println("Ejecutando metodo");
+    try 
+    {
+        Thread.sleep(4000);
+        System.out.println("Paso sleep");
+        // You must use a builder to construct a new Protobuffer object
+        GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
+          .setGreeting("Hello there, " + request.getName())
+          .build();
+
+        // Use responseObserver to send a single response back
+        responseObserver.onNext(response);
+
+        // When you are done, you must call onCompleted.
+        responseObserver.onCompleted();
+    } 
+    catch(InterruptedException e)
+    {
+      System.out.println(e);
+    } 
+  }
+}
